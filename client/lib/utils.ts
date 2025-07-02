9a50e2fc49a828ad57a7dc219f30e68b380de1dc
theo-guerin <theo-guerin@noreply.codeberg.org>
2025-07-03 01:58:00 +0200

    Add BadgeLoader component and implement alphanumeric validation for login

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isAlphanum(str: string): boolean {
+  return /^[a-z0-9]+$/i.test(str);
+}
