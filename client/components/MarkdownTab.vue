ea3f084c50fb8eed4735fa0da9a760dee7f63a51
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2025-07-02 22:17:53 +0200

    Update dependencies and add tab components for HTML and Markdown display

---
+++ client/components/MarkdownTab.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  apiUrl: string;
+}>();
+
+const code = computed<string>(() => {
+  return `[![ftbadge](${props.apiUrl})](https://ftbadge.cc)`;
+});
+</script>
+
+<template>
+  <div class="rounded-md border border-gray-700 bg-gray-800 p-4">
+    <code class="text-sm break-all text-orange-400">{{ code }}</code>
+  </div>
+  <CopyButton label="Markdown" :copyText="code" class="mt-3" />
+</template>
