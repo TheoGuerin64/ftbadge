63ee0eb3fa68322745c53ae86d86e8a5eb1d4692
theo-guerin <theo-guerin@noreply.codeberg.org>
2025-12-31 16:54:49 +0100

    feat: add no-cache header to health check
    Adds a `Cache-Control` header to the health check endpoint to
    prevent caching.

--- api/internal/handlers/health.go
+++ api/internal/handlers/health.go
@@ -7,6 +7,7 @@ import (
 )
 
 func HealthCheckHandler(ctx echo.Context) error {
+	ctx.Response().Header().Set("Cache-Control", "no-store, no-cache, max-age=0")
 	data := map[string]string{"status": "ok"}
 	return ctx.JSON(http.StatusOK, data)
 }
