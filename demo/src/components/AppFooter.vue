2a630fe1cbca55c3396ae70eef4738208a0487e5
theo-guerin <theo-guerin@noreply.codeberg.org>
2025-12-31 18:52:14 +0100

    chore: update copyright year in AppFooter
    Updates the copyright year to 2026 in the AppFooter component.

--- demo/src/components/AppFooter.vue
+++ demo/src/components/AppFooter.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-6xl px-4 py-8">
       <div class="text-center text-gray-400">
         <p>
-          &copy; 2025 ftbadge. Released under the MIT License.
+          &copy; 2026 ftbadge. Released under the MIT License.
           <a
             href="https://opensource.org/licenses/MIT"
             target="_blank"
